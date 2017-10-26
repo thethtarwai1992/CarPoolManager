c72b38228a88f0f795ab992016d13b6d3cc86473
thethtarwai <blackfirewave@gmail.com>
2017-10-26 22:45:55 +0800

    Update sql

--- carpooling.sql
+++ carpooling.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 20, 2017 at 04:02 PM
+-- Generation Time: Oct 22, 2017 at 10:34 AM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.9
 
@@ -19,52 +19,65 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `mydb`
+-- Database: `carpoolingmanager`
 --
+
+DROP TABLE IF EXISTS `bookings`;
+DROP TABLE IF EXISTS `cars`;
+DROP TABLE IF EXISTS `drivers`;
+DROP TABLE IF EXISTS `driver_cars`;
+DROP TABLE IF EXISTS `locations`;
+DROP TABLE IF EXISTS `preferences`;
+DROP TABLE IF EXISTS `routes`;
+DROP TABLE IF EXISTS `users`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `booking`
+-- Table structure for table `bookings`
 --
 
 CREATE TABLE `bookings` (
   `booking_id` int(11) NOT NULL,
-  `request_time` datetime DEFAULT NULL,
+  `request_time` datetime NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `price` decimal(2,0) DEFAULT NULL,
+  `drivers_driving_license_no` varchar(45) NOT NULL,
   `passenger_id` int(11) NOT NULL,
-  `status_id` int(11) NOT NULL,
-  `route_id` int(11) NOT NULL,
-  `price` decimal(2,0) DEFAULT NULL
+  `route_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `car`
+-- Table structure for table `cars`
 --
 
 CREATE TABLE `cars` (
   `plate_no` int(11) NOT NULL,
-  `model` varchar(45) DEFAULT NULL,
-  `manufacture_year` varchar(45) DEFAULT NULL
+  `model` varchar(45) NOT NULL,
+  `manufacture_year` varchar(45) NOT NULL,
+  `capacity` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `driver`
+-- Table structure for table `drivers`
 --
 
 CREATE TABLE `drivers` (
   `driving_license_no` varchar(45) NOT NULL,
-  `driving_license_valid_from` date DEFAULT NULL,
-  `User_userID` int(11) NOT NULL
+  `driving_license_valid_from` date NOT NULL,
+  `status` varchar(15) NOT NULL,
+  `remarks` varchar(100) DEFAULT NULL,
+  `userID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `driver_car`
+-- Table structure for table `driver_cars`
 --
 
 CREATE TABLE `driver_cars` (
@@ -76,22 +89,23 @@ CREATE TABLE `driver_cars` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `location`
+-- Table structure for table `locations`
 --
 
 CREATE TABLE `locations` (
+  `location_id` int(11) NOT NULL,
   `address` varchar(100) NOT NULL,
-  `postal_code` int(6) NOT NULL,
-  `location_id` int(11) NOT NULL
+  `postal_code` int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `preference`
+-- Table structure for table `preferences`
 --
 
 CREATE TABLE `preferences` (
+  `preference_id` int(8) NOT NULL,
   `userID` int(11) NOT NULL,
   `total_pax` int(11) DEFAULT NULL,
   `tripdate` date DEFAULT NULL,
@@ -102,7 +116,7 @@ CREATE TABLE `preferences` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `route`
+-- Table structure for table `routes`
 --
 
 CREATE TABLE `routes` (
@@ -120,137 +134,158 @@ CREATE TABLE `routes` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `status`
---
-
-CREATE TABLE `status` (
-  `status_id` int(11) NOT NULL,
-  `status_desc` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `userID` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
   `first_name` varchar(45) NOT NULL,
   `last_name` varchar(45) DEFAULT NULL,
-  `gender` varchar(45) NOT NULL,
+  `gender` varchar(1) DEFAULT NULL,
   `email` varchar(100) NOT NULL,
   `contactNO` int(8) NOT NULL,
-  `password` varchar(100) NOT NULL
+  `password` varchar(100) NOT NULL,
+  `is_driver` int(11) NOT NULL DEFAULT '0',
+  `remember_token` text,
+  `created_at` datetime NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userID`,`name`, `first_name`, `last_name`, `gender`, `email`, `contactNO`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'Thet Htar', 'Thet', NULL, 'F', 'thethw001@mymail.sim.edu.sg', 11111111, '$2y$10$Gnk1GGQ/rmaOqVYEQCJb1u299A2KLNk4mZsgnh1ZIgbn/enmQfn1y', '2017-10-22 08:06:46', '2017-10-22 00:06:46'),
+(2, 'Yuting', 'Yuting', 'Wang', 'F', 'ywang084@mymail.sim.edu.sg', 22222222, '$2y$10$Gnk1GGQ/rmaOqVYEQCJb1u299A2KLNk4mZsgnh1ZIgbn/enmQfn1y', '2017-10-22 08:06:46', '2017-10-22 00:06:46'),
+(3, 'Min Thu', 'Min Thu', NULL, 'F', 'yinmt001@mymail.sim.edu.sg', 33333333, '$2y$10$Gnk1GGQ/rmaOqVYEQCJb1u299A2KLNk4mZsgnh1ZIgbn/enmQfn1y', '2017-10-22 08:06:46', '2017-10-22 00:06:46');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `booking`
+-- Indexes for table `bookings`
 --
-ALTER TABLE `booking`
-  ADD PRIMARY KEY (`booking_id`,`passenger_id`,`status_id`,`route_id`),
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`booking_id`),
   ADD KEY `fk_booking_User1_idx` (`passenger_id`),
-  ADD KEY `fk_booking_status1_idx` (`status_id`),
-  ADD KEY `fk_booking_route1_idx` (`route_id`);
+  ADD KEY `fk_booking_route1_idx` (`route_id`),
+  ADD KEY `fk_bookings_drivers1_idx` (`drivers_driving_license_no`);
 
 --
--- Indexes for table `car`
+-- Indexes for table `cars`
 --
-ALTER TABLE `car`
+ALTER TABLE `cars`
   ADD PRIMARY KEY (`plate_no`);
 
 --
--- Indexes for table `driver`
+-- Indexes for table `drivers`
 --
-ALTER TABLE `driver`
+ALTER TABLE `drivers`
   ADD PRIMARY KEY (`driving_license_no`),
-  ADD KEY `fk_driver_User_idx` (`User_userID`);
+  ADD KEY `fk_driver_User_idx` (`userID`);
 
 --
--- Indexes for table `driver_car`
+-- Indexes for table `driver_cars`
 --
-ALTER TABLE `driver_car`
+ALTER TABLE `driver_cars`
   ADD PRIMARY KEY (`driver_driving_license_no`,`car_plate_no`),
   ADD KEY `fk_driver_has_car_car1_idx` (`car_plate_no`),
   ADD KEY `fk_driver_has_car_driver1_idx` (`driver_driving_license_no`);
 
 --
--- Indexes for table `location`
+-- Indexes for table `locations`
 --
-ALTER TABLE `location`
+ALTER TABLE `locations`
   ADD PRIMARY KEY (`location_id`),
   ADD UNIQUE KEY `postal_code_UNIQUE` (`postal_code`);
 
 --
--- Indexes for table `preference`
+-- Indexes for table `preferences`
 --
-ALTER TABLE `preference`
-  ADD PRIMARY KEY (`userID`),
+ALTER TABLE `preferences`
+  ADD PRIMARY KEY (`preference_id`),
+  ADD UNIQUE KEY `preference_id_UNIQUE` (`preference_id`),
   ADD KEY `fk_Preference_User1_idx` (`userID`);
 
 --
--- Indexes for table `route`
+-- Indexes for table `routes`
 --
-ALTER TABLE `route`
+ALTER TABLE `routes`
   ADD PRIMARY KEY (`route_id`,`driver_car_driver_driving_license_no`,`driver_car_car_plate_no`),
   ADD KEY `fk_route_driver_car1_idx` (`driver_car_driver_driving_license_no`,`driver_car_car_plate_no`),
   ADD KEY `fk_route_Location1_idx` (`start_point`),
   ADD KEY `fk_route_Location2_idx` (`destination`);
 
 --
--- Indexes for table `status`
+-- Indexes for table `users`
 --
-ALTER TABLE `status`
-  ADD PRIMARY KEY (`status_id`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userID`),
+  ADD UNIQUE KEY `userID_UNIQUE` (`userID`);
 
 --
--- Indexes for table `user`
+-- AUTO_INCREMENT for dumped tables
 --
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`userID`);
+
+--
+-- AUTO_INCREMENT for table `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `locations`
+--
+ALTER TABLE `locations`
+  MODIFY `location_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `booking`
+-- Constraints for table `bookings`
 --
-ALTER TABLE `booking`
-  ADD CONSTRAINT `fk_booking_User1` FOREIGN KEY (`passenger_id`) REFERENCES `user` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_booking_route1` FOREIGN KEY (`route_id`) REFERENCES `route` (`route_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_booking_status1` FOREIGN KEY (`status_id`) REFERENCES `status` (`status_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `fk_booking_User1` FOREIGN KEY (`passenger_id`) REFERENCES `users` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_booking_route1` FOREIGN KEY (`route_id`) REFERENCES `routes` (`route_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_bookings_drivers1` FOREIGN KEY (`drivers_driving_license_no`) REFERENCES `drivers` (`driving_license_no`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Constraints for table `driver`
+-- Constraints for table `drivers`
 --
-ALTER TABLE `driver`
-  ADD CONSTRAINT `fk_driver_User` FOREIGN KEY (`User_userID`) REFERENCES `user` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `drivers`
+  ADD CONSTRAINT `fk_driver_User` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Constraints for table `driver_car`
+-- Constraints for table `driver_cars`
 --
-ALTER TABLE `driver_car`
-  ADD CONSTRAINT `fk_driver_has_car_car1` FOREIGN KEY (`car_plate_no`) REFERENCES `car` (`plate_no`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_driver_has_car_driver1` FOREIGN KEY (`driver_driving_license_no`) REFERENCES `driver` (`driving_license_no`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `driver_cars`
+  ADD CONSTRAINT `fk_driver_has_car_car1` FOREIGN KEY (`car_plate_no`) REFERENCES `cars` (`plate_no`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_driver_has_car_driver1` FOREIGN KEY (`driver_driving_license_no`) REFERENCES `drivers` (`driving_license_no`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Constraints for table `preference`
+-- Constraints for table `preferences`
 --
-ALTER TABLE `preference`
-  ADD CONSTRAINT `fk_Preference_User1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `preferences`
+  ADD CONSTRAINT `fk_Preference_User1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Constraints for table `route`
+-- Constraints for table `routes`
 --
-ALTER TABLE `route`
-  ADD CONSTRAINT `fk_route_Location1` FOREIGN KEY (`start_point`) REFERENCES `location` (`postal_code`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_route_Location2` FOREIGN KEY (`destination`) REFERENCES `location` (`location_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_route_driver_car1` FOREIGN KEY (`driver_car_driver_driving_license_no`,`driver_car_car_plate_no`) REFERENCES `driver_car` (`driver_driving_license_no`, `car_plate_no`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `routes`
+  ADD CONSTRAINT `fk_route_Location1` FOREIGN KEY (`start_point`) REFERENCES `locations` (`postal_code`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_route_Location2` FOREIGN KEY (`destination`) REFERENCES `locations` (`location_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_route_driver_car1` FOREIGN KEY (`driver_car_driver_driving_license_no`,`driver_car_car_plate_no`) REFERENCES `driver_cars` (`driver_driving_license_no`, `car_plate_no`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
